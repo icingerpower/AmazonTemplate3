1d1443809b08ce99731582cdf4a701b0ed67cada
cedric <icingerpower@gmail.com>
2025-12-26 09:52:02 +0100

    Designed the main window with implementatiions of basic controls.

--- AmazonTemplate3/gui/MainWindow.cpp
+++ AmazonTemplate3/gui/MainWindow.cpp
@@ -1,3 +1,10 @@
+#include <QFileDialog>
+
+#include <TemplateFiller.h>
+#include <FileModelToFill.h>
+#include <FileModelSources.h>
+#include <TemplateFiller.h>
+
 #include "MainWindow.h"
 #include "./ui_MainWindow.h"
 
@@ -5,11 +12,125 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    m_templateFiller = nullptr;
     ui->setupUi(this);
+    QString m_settingsKeyExtraInfos;
+    QString m_settingsKeyApi;
+    _connectSlots();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    _clearTemplateFiller();
+}
+
+void MainWindow::_connectSlots()
+{
+    connect(ui->buttonBrowseSource,
+            &QPushButton::clicked,
+            this,
+            &MainWindow::browseSourceMain);
+}
+
+void MainWindow::_clearTemplateFiller()
+{
+    if (m_templateFiller != nullptr)
+    {
+        delete m_templateFiller;
+    }
+}
+
+void MainWindow::browseSourceMain()
+{
+    QSettings settings;
+    const QString key{"MainWindow__browseSourceMain"};
+    QDir lastDir{settings.value(key, QDir{}.path()).toString()};
+    const QString &filePath = QFileDialog::getOpenFileName(
+        this,
+        tr("Template file pre-filled"),
+        lastDir.path(),
+        QString{"Xlsx (*TOFILL*.xlsx *TOFILL*.XLSX *TOFILL*.xlsm *TOFILL*.XLSM)"},
+        nullptr,
+        QFileDialog::DontUseNativeDialog);
+    if (!filePath.isEmpty())
+    {
+        _clearTemplateFiller();
+        m_workingDir = QFileInfo{filePath}.dir();
+        m_settingsFilePath = m_workingDir.absoluteFilePath("settings.ini");
+        auto settingsDir = settingsFolder();
+        if (settingsDir->contains(m_settingsKeyExtraInfos))
+        {
+            ui->textEditExtraInfos->setText(
+                        settingsDir->value(m_settingsKeyExtraInfos).toString());
+        }
+        else
+        {
+            ui->textEditExtraInfos->clear();
+        }
+        ui->buttonExtractProductInfos->setEnabled(true);
+        const auto &workingDirPath = m_workingDir.path();
+        settings.setValue(key, workingDirPath);
+        ui->lineEditTo->setText(filePath);
+        _enableGenerateButtonIfValid();
+        auto *curModelSource = ui->treeViewSources->model();
+        auto *fileModelSources
+            = new FileModelSources{workingDirPath, ui->treeViewSources};
+        ui->treeViewSources->setModel(fileModelSources);
+        ui->treeViewSources->setRootIndex(
+            fileModelSources->index(workingDirPath));
+        ui->treeViewSources->header()->resizeSection(0, 300);
+        auto *curModelToFill = ui->treeViewToFill->model();
+        auto *fileModelToFill
+            = new FileModelToFill{workingDirPath, ui->treeViewToFill};
+        m_templateFiller = new TemplateFiller{filePath, fileModelToFill->getFilePaths()};
+        ui->treeViewToFill->setModel(fileModelToFill);
+        ui->treeViewToFill->setRootIndex(fileModelToFill->index(workingDirPath));
+        ui->treeViewToFill->header()->resizeSection(0, 300);
+        if (curModelToFill != nullptr)
+        {
+            curModelToFill->deleteLater();
+            curModelSource->deleteLater();
+        }
+    }
+    m_templateFiller = nullptr;
+}
+
+QSharedPointer<QSettings> MainWindow::settingsFolder() const
+{
+    return QSharedPointer<QSettings>{new QSettings{m_settingsFilePath, QSettings::IniFormat}};
+}
+
+void MainWindow::_enableGenerateButtonIfValid()
+{
+    if (!ui->lineEditOpenAiKey->text().isEmpty()
+        && !ui->lineEditTo->text().isEmpty())
+    {
+        _setGenerateButtonsEnabled(true);
+    }
+    else
+    {
+        _setGenerateButtonsEnabled(false);
+    }
+}
+
+void MainWindow::_setGenerateButtonsEnabled(bool enable)
+{
+    ui->buttonGenerate->setEnabled(enable);
+    ui->buttonBasicControls->setEnabled(enable);
+    ui->buttonGenAiDesc->setEnabled(enable);
+    ui->buttonReviewAiDesc->setEnabled(enable);
+    ui->buttonDisplayPossibleValues->setEnabled(enable);
+    ui->buttonRunPromptsManually->setEnabled(enable);
+}
+
+void MainWindow::baseControls()
+{
+
+}
+
+void MainWindow::extractProductInfos()
+{
+
 }
 
--- AmazonTemplate3/gui/MainWindow.h
+++ AmazonTemplate3/gui/MainWindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDir>
+#include <QSettings>
+#include <QSharedPointer>
+
+class TemplateFiller;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +20,24 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    QSharedPointer<QSettings> settingsFolder() const;
+
+public slots:
+    void browseSourceMain();
+    void baseControls();
+    void extractProductInfos();
+
 private:
     Ui::MainWindow *ui;
+    void _connectSlots();
+    TemplateFiller *m_templateFiller;
+    QDir m_workingDir;
+    QString m_settingsFilePath;
+    QString m_settingsKeyExtraInfos;
+    QString m_settingsKeyApi;
+    void _clearTemplateFiller();
+    void _setGenerateButtonsEnabled(bool enable);
+    void _enableGenerateButtonIfValid();
 };
+
 #endif // MAINWINDOW_H
